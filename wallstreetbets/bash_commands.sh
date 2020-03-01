540da562e599a8e32e168f603fcfd057a5426b0c
Bryan Ly <bryan.ly@kp.org>
2020-02-29 22:36:12 -0800

    Externalizing credentials

---
+++ wallstreetbets/bash_commands.sh
@@ -0,0 +1,23 @@
+# Created an alias inside .bash_profile to default to python3
+vi .bash_profile
+    - alias python="python3"
+
+# Updated pip and then used pip3 to install SSL packages
+pip install --upgrade pip
+pip install --upgrade pyOpenSSL
+
+# Install Google search python library, which installed it for python 2.7...
+pip install google
+
+# Troubleshooting to install Google search for python 3.x...
+# https://stackoverflow.com/questions/41328451/ssl-module-in-python-is-not-available-when-installing-package-with-pip3
+brew update && brew upgrade
+brew uninstall --ignore-dependencies openssl
+brew install openssl
+
+pip3 install google, worked!
+
+
+###~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Wall Street Bets ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~###
+# Helpful Links:
+# https://stackoverflow.com/questions/4383571/importing-files-from-different-folder
---
+++ wallstreetbets/reddit_scraper.py
@@ -0,0 +1,24 @@
+# Implement a reddit web-scraper that analyzes if there's an effect between sub-reddit WallStreetBets vs. Stock Prices
+# Specifically, we will be analyzing users who uses the word Bear vs. Bull to determine if the market
+# will either move in an up-trend or down-trend because of the commonality of these words
+
+
+import praw
+#import requests
+#import requests.auth
+import sys
+sys.path.insert(1, '/Users/bryan/Documents/Configurations/reddit')
+import redditconf
+
+reddit = praw.Reddit(client_id=redditconf.client_id[0],
+                    client_secret=redditconf.client_secret[0],
+                    password=redditconf.password[0],
+                    user_agent=redditconf.user_agent[0],
+                    username=redditconf.username[0])
+
+# Printing out who is the current reddit user
+print("Scraping from User: " + str(reddit.user.me()))
+
+hot_posts = reddit.subreddit('wallstreetbets').hot(limit=10)
+for post in hot_posts:
+    print(post.title)
